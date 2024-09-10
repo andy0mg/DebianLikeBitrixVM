438f5de339ef5b0175ce4c49ce8b6b8ae7d3c884
andy0mg <40331027+andy0mg@users.noreply.github.com>
2024-09-10 20:39:18 +0300

    Update custom_from_install_full_environment_bitrix_setup_vanilla.sh

--- repositories/bitrix-gt/custom_from_install_full_environment_bitrix_setup_vanilla.sh
+++ repositories/bitrix-gt/custom_from_install_full_environment_bitrix_setup_vanilla.sh
@@ -201,7 +201,8 @@ EOF
 }
 
 dplNginx(){
-		rm /etc/nginx/sites-enabled/default
+[ -e /etc/nginx/sites-enabled/default ] && rm /etc/nginx/sites-enabled/default
+#  rm /etc/nginx/sites-enabled/default
 	ln -s /etc/nginx/sites-available/rtc.conf /etc/nginx/sites-enabled/rtc.conf
 	ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
 	systemctl stop nginx
