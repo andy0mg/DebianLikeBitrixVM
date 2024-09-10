60dd03c315ccda62c3a9db330108ec546a2db36e
andy0mg <40331027+andy0mg@users.noreply.github.com>
2024-09-10 16:03:07 +0300

    Update install_full_environment.sh

--- install_full_environment.sh
+++ install_full_environment.sh
@@ -46,7 +46,7 @@ FULL_PATH_MENU_FILE="$DEST_DIR_MENU/$DIR_NAME_MENU/menu.sh"
 apt update -y
 apt upgrade -y
 apt install -y perl wget curl ansible git ssl-cert cron locales locales-all poppler-utils catdoc
-ansible-galaxy --ignore-certs collection install community.rabbitmq --force
+ANSIBLE_GALAXY_IGNORE=true ansible-galaxy collection install --ignore-certs community.rabbitmq --force
 # TODO find how to install  - libnginx-mod-http-brotli-filter libnginx-mod-http-brotli-static
 
 # Set locales
