de538ffdbdadfdabc963017b8e48cfbbf97b5e9f
andy0mg <40331027+andy0mg@users.noreply.github.com>
2024-09-12 12:49:30 +0300

    Update update_astra.sh

--- update_astra.sh
+++ update_astra.sh
@@ -5,5 +5,5 @@ echo "deb http://dl.astralinux.ru/astra/frozen/1.7_x86-64/1.7.6/repository-main/
 echo "deb http://dl.astralinux.ru/astra/frozen/1.7_x86-64/1.7.6/repository-update/ 1.7_x86-64 main contrib non-free" >> /etc/apt/sources.list
 apt update
 astra-update -A -r
-# use curl
+# use curl#
 # bash <(curl -ksL https://raw.githubusercontent.com/andy0mg/DebianLikeBitrixVM/master/update_astra.sh)
